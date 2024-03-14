99f1cf0c51581723b5b1ce835b0e6cc577a5c3f2
parkjh <jaeheep99@gmail.com>
2024-03-14 13:53:00 +0900

    master -> main rebased

---
+++ src/main/java/com/t2m/g2nee/front/FrontApplication.java
@@ -0,0 +1,15 @@
+package com.t2m.g2nee.front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// front server application
+
+@SpringBootApplication
+public class FrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontApplication.class, args);
+	}
+
+}
