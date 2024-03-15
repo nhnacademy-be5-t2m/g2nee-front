a0cf06b6cba36fffae458451fbd1410cea73895c
정지은 <68415975+ssstopeun@users.noreply.github.com>
2024-03-15 17:22:31 +0900

    Update TestController.java

--- src/main/java/com/t2m/g2nee/front/TestController.java
+++ src/main/java/com/t2m/g2nee/front/TestController.java
@@ -5,12 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TestController {
 
-    @GetMapping(value={"/index.html","/"})
+    @GetMapping("/")
     public String hello() {
 
-        return "index/index";
+        return "Hello Test!";
     }
 }
