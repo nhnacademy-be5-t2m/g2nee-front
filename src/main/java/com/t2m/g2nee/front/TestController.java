e411a75f877faf0a131cd998bbff5ea9ad056776
KimSu-Been <89886506+KimSu-Been@users.noreply.github.com>
2024-03-15 16:55:35 +0900

    Update TestController.java

--- src/main/java/com/t2m/g2nee/front/TestController.java
+++ src/main/java/com/t2m/g2nee/front/TestController.java
@@ -5,12 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class TestController {
 
-    @GetMapping("/")
+    @GetMapping(value={"/index.html","/"})
     public String hello() {
 
-        return "Hello Test!";
+        return "index/index";
     }
 }
