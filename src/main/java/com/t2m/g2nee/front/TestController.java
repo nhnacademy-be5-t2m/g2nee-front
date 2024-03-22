c25484e05eeedbe42a08230fc8e0f10ea6b8f9bb
정지은 <68415975+ssstopeun@users.noreply.github.com>
2024-03-22 16:52:32 +0900

    :sparkles: add test
    shop의 RestAPI 요청을 보내 받아오는 과정 Test
    (front -> gateway -> shop)

--- src/main/java/com/t2m/g2nee/front/TestController.java
+++ src/main/java/com/t2m/g2nee/front/TestController.java
@@ -1,16 +1,50 @@
 package com.t2m.g2nee.front;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 
 @RestController
 public class TestController {
 
-    @GetMapping("/test")
-    public String hello() {
+    RestTemplate restTemplate;
+    ObjectMapper objectMapper;
 
-        return "hello g2nee-front!";
+    @Autowired
+    public TestController(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = new RestTemplateBuilder()
+                .requestFactory(() -> new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()))
+                .setConnectTimeout(Duration.ofMillis(5L * 1000))
+                .setReadTimeout(Duration.ofMillis(5L * 1000))
+                .additionalMessageConverters(new StringHttpMessageConverter(StandardCharsets.UTF_8))
+                .build();
+        objectMapper = new ObjectMapper();
+    }
+
+    @GetMapping("/test")
+    public String goToShopTest() {
+        ResponseEntity<String> testResult = restTemplate.exchange(
+                "http://133.186.208.183/shop/hello",
+                HttpMethod.GET,
+                null,
+                String.class
+        );
+
+        return testResult.getBody();
     }
 }
