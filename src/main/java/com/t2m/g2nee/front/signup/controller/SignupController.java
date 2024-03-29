9ee9b4d37a8d0b7205a85ed7c0f0f77737725c24
정지은 <68415975+ssstopeun@users.noreply.github.com>
2024-03-29 18:39:51 +0900

    :sparkles: signup 기본페이지 작성

---
+++ src/main/java/com/t2m/g2nee/front/signup/controller/SignupController.java
@@ -0,0 +1,16 @@
+package com.t2m.g2nee.front.signup.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/signup")
+public class SignupController {
+
+    @GetMapping
+    public String signup(){
+        return "/member/signUpPage";
+    }
+
+}
