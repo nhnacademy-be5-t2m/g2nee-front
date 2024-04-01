f25314b81fc5bf1536c892614346c75843c5ad43
정지은 <68415975+ssstopeun@users.noreply.github.com>
2024-04-01 15:52:35 +0900

    :sparkles: signup page

---
+++ src/main/java/com/t2m/g2nee/front/signup/controller/SignupController.java
@@ -0,0 +1,22 @@
+package com.t2m.g2nee.front.signup.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/signup")
+public class SignupController {
+
+    @GetMapping
+    public String signup(){
+        return "/member/signupPage";
+    }
+
+    @PostMapping
+    public String signupComplate(){
+
+        return "/member/signupComplate";
+    }
+}
