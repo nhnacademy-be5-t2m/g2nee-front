9c4d034f27777e2d94feaada1ceeb8e1d26f6b2f
정지은 <68415975+ssstopeun@users.noreply.github.com>
2024-04-05 13:35:15 +0900

    :recycle: 회원가입성공페이지 수정

--- src/main/java/com/t2m/g2nee/front/handler/ErrorHandler.java
+++
@@ -1,6 +0,0 @@
-//package com.t2m.g2nee.front.handler;
-//
-//import org.springframework.web.client.DefaultResponseErrorHandler;
-//
-//public class ErrorHandler extends DefaultResponseErrorHandler {
-//}
