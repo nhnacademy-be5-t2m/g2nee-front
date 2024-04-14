6329ea26b10b1d2f3c04d08bc12ca2ba15661c9b
KimSu-Been <tnqls631@naver.com>
2024-04-15 02:36:22 +0900

    :wrench: 설정 수정

---
+++ src/main/java/com/t2m/g2nee/front/config/CachingConfig.java
@@ -0,0 +1,20 @@
+package com.t2m.g2nee.front.config;
+
+import java.util.List;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@EnableCaching
+@Configuration
+public class CachingConfig {
+    @Bean
+    public CacheManager cacheManager() {
+        ConcurrentMapCacheManager cacheManager = new ConcurrentMapCacheManager();
+        cacheManager.setAllowNullValues(false);
+        cacheManager.setCacheNames(List.of("categories"));
+        return cacheManager;
+    }
+}
