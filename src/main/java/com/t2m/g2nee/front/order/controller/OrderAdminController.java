774c3f49b270c775b6caebd3232c0dc088503e16
parkjh <jaeheep99@gmail.com>
2024-04-16 14:51:20 +0900

    :construction: order

---
+++ src/main/java/com/t2m/g2nee/front/order/controller/OrderAdminController.java
@@ -0,0 +1,24 @@
+package com.t2m.g2nee.front.order.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class OrderAdminController {
+    @GetMapping("/order")
+    public String order(){
+        return "orders/order";
+    }
+    @GetMapping("/order-testing")
+    public String ordertest(){
+        return "orders/order-test";
+    }
+    @GetMapping("/order/{orderId}")
+    public String orderInfo(){
+        return "orders/order";
+    }
+    @GetMapping("/admin/order")
+    public String orderList(){
+        return "admin/adminOrder";
+    }
+}
