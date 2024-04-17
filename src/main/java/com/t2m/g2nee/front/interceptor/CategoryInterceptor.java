e6533f80150e58861b186683a9c789e6bf84873d
KimSu-Been <tnqls631@naver.com>
2024-04-17 11:09:09 +0900

    :recycle: 카테고리 프론트 수정

--- src/main/java/com/t2m/g2nee/front/interceptor/CategoryInterceptor.java
+++ src/main/java/com/t2m/g2nee/front/interceptor/CategoryInterceptor.java
@@ -1,10 +1,8 @@
 package com.t2m.g2nee.front.interceptor;
 
-import com.t2m.g2nee.front.category.dto.response.CategoryHierarchyDto;
-import com.t2m.g2nee.front.category.dto.response.CategoryInfoDto;
+
 import com.t2m.g2nee.front.category.service.CategoryService;
 import java.util.Collections;
-import java.util.List;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
