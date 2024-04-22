5926721ecc12e1c02e3be9eb5484b1ddefeee6ab
정지은 <68415975+ssstopeun@users.noreply.github.com>
2024-04-22 16:26:12 +0900

    Update WebMvcConfig.java

--- src/main/java/com/t2m/g2nee/front/config/WebMvcConfig.java
+++ src/main/java/com/t2m/g2nee/front/config/WebMvcConfig.java
@@ -30,6 +30,5 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
-        registry.addResourceHandler("/templates/**").addResourceLocations("classpath:/templates/");
     }
 }
