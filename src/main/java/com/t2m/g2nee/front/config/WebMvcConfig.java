5c38261ebe33a32e785e5939bd94ae81326dfbcc
정지은 <68415975+ssstopeun@users.noreply.github.com>
2024-04-22 16:17:21 +0900

    :bug: template 경로 추가

--- src/main/java/com/t2m/g2nee/front/config/WebMvcConfig.java
+++ src/main/java/com/t2m/g2nee/front/config/WebMvcConfig.java
@@ -30,5 +30,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/templates/**").addResourceLocations("classpath:/templates/");
     }
 }
