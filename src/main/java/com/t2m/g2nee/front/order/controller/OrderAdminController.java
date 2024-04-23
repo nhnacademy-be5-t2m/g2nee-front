5569f133eee5b6e8a94d67fa5ef40b3d3a9580ed
parkjh <jaeheep99@gmail.com>
2024-04-23 17:10:10 +0900

    :construction: adminOrder.html construction

--- src/main/java/com/t2m/g2nee/front/order/controller/OrderAdminController.java
+++ src/main/java/com/t2m/g2nee/front/order/controller/OrderAdminController.java
@@ -5,20 +5,5 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class OrderAdminController {
-    @GetMapping("/order")
-    public String order(){
-        return "orders/order";
-    }
-    @GetMapping("/order-testing")
-    public String ordertest(){
-        return "orders/order-test";
-    }
-    @GetMapping("/order/{orderId}")
-    public String orderInfo(){
-        return "orders/order";
-    }
-    @GetMapping("/admin/order")
-    public String orderList(){
-        return "admin/adminOrder";
-    }
+
 }
