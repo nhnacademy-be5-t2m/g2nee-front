4f0666f1676fddd8c41774a304a283c58fed1797
KimSu-Been <tnqls631@naver.com>
2024-05-12 22:47:01 +0900

    :recycle: 머지

---
+++ src/main/java/com/t2m/g2nee/front/payment/controller/MypagePaymentController.java
@@ -0,0 +1,30 @@
+package com.t2m.g2nee.front.payment.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MypagePaymentController {
+
+//    private final MemberAspect memberAspect;
+//
+//    /**
+//     * 한 고객의 결제 목록
+//     */
+//    @GetMapping("/payments")
+//    public String payments(Model model, int page){
+//        Long memberId = (Long) memberAspect.getThreadLocal().get();
+//        model.addAttribute("payments", paymentService.getPayments(memberId, page));
+//    }
+
+    /**
+     * 주문 id에 따른 결제 내역
+     */
+
+//    /**
+//     * 결제 취소
+//     */
+//    @PostMapping("/{paymentId}")
+//    public String paymentCancel(){
+//        return "";
+//    }
+}
