4f6e93581e29bdafc87695bfd29d7033c54d3d45
ehdals0405 <aokagami@naver.com>
2024-05-17 14:58:10 +0900

    :sparkles: 재고 추가 기능 추가, admin 책 목록 viewer 추가

--- src/main/java/com/t2m/g2nee/front/bookset/book/controller/BookRestController.java
+++ src/main/java/com/t2m/g2nee/front/bookset/book/controller/BookRestController.java
@@ -2,13 +2,17 @@ package com.t2m.g2nee.front.bookset.book.controller;
 
 import com.t2m.g2nee.front.bookset.book.dto.BookDto;
 import com.t2m.g2nee.front.bookset.book.service.BookGetService;
+import com.t2m.g2nee.front.bookset.book.service.BookMgmtService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookRestController {
 
     private final BookGetService bookGetService;
+    private final BookMgmtService bookMgmtService;
 
     @PostMapping("/checkStock")
     public ResponseEntity<List<BookDto.ListResponse>> getBookExceedStock(
@@ -34,5 +39,13 @@ public class BookRestController {
         List<BookDto.ListResponse> responses = bookGetService.getModifiedPriceBook(bookList);
         return ResponseEntity.status(HttpStatus.OK).body(responses);
 
+    }
+
+    @PatchMapping("/{bookId}")
+    public ResponseEntity<Integer> modifyBookQuantity(@PathVariable Long bookId, @RequestParam int quantity){
+
+        Integer modifiedQuantity = bookMgmtService.addBookQuantity(bookId,quantity);
+
+        return ResponseEntity.ok(modifiedQuantity);
     }
 }
